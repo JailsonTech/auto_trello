54c59b4c1875e84229a12d50d4fe23c4062e594c
Jailson <jailsonrodriguestech@gmail.com>
2024-07-10 01:10:00 -0300

    ajustes 2

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from trello import TrelloClient
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+import pytz
+
 
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -27,13 +29,23 @@ print(f'\nNome do cartão: {card.name}')
 print(f'Descrição do cartão: {card.description}')
 print(f'URL do cartão: {card.shortUrl}')
 
+# Definir o fuso horário de Salvador
+salvador_timezone = pytz.timezone('America/Bahia')
+
 # Mostrar a data do cartão, se existir
 if card.due_date:
-    formatted_due_date = card.due_date.strftime('%d/%m/%Y %H:%M:%S')
+    if isinstance(card.due_date, datetime):
+        due_date_salvador = card.due_date.astimezone(salvador_timezone)
+    else:
+        due_date_utc = datetime.strptime(card.due_date, '%Y-%m-%dT%H:%M:%S.%fZ')
+        due_date_utc = due_date_utc.replace(tzinfo=pytz.utc)
+        due_date_salvador = due_date_utc.astimezone(salvador_timezone)
+    
+    formatted_due_date = due_date_salvador.strftime('%d/%m/%Y')
     print(f'Data do cartão: {formatted_due_date}')
 else:
     print('Este cartão não possui uma data definida.')
-
+    
 # Verificar se há checklists no cartão
 if card.checklists:
     # Iterar sobre os checklists do cartão
